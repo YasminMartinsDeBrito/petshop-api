95d2da5ffd443d4e83704dea0e105aea513cc22d
yasmin <yasminmartinsdebrito@gmail.com>
2022-08-15 11:02:00 -0300

    feat:criar repositories de users

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,37 @@
+import { DeleteResult, Repository, UpdateResult } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/User";
+
+interface IUserRepo {
+    save: (user:Partial<User>) => Promise<User>
+    all: () => Promise<User[]>
+    findOne: (payload:object) => Promise<User>
+    update: (id:string, payload: Partial<User>) => Promise<UpdateResult>
+    delete: (id:string) => Promise<DeleteResult>
+}
+
+class UserRepo implements IUserRepo {
+    private ormRepo: Repository<User>
+
+    constructor(){
+        this.ormRepo = AppDataSource.getRepository(User)
+    }
+
+    save = async(user:Partial<User>) => {
+        return await this.ormRepo.save(user)
+    }
+    all = async() => {
+        return await this.ormRepo.find()
+    }
+    findOne = async(payload:object) => {
+        return await this.ormRepo.findOneBy({...payload})
+    }
+    update = async(userId:string, payload:Partial<User>) => {
+        return await this.ormRepo.update(userId, {...payload})
+    }
+    delete = async(userId:string) => {
+        return await this.ormRepo.delete(userId)
+    }
+}
+
+export default new UserRepo()
